172eb980617178f6e23d49af7d933a9e603ea858
Navneet Golwalkar <navneet@akpsolutionsinc.com>
2022-07-25 17:27:03 +0530

    changes in code

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ const port = process.env.PORT || 3000;
 const express = require('express');
 const router = express.Router();
 
-const server = http.createServer((req, res) => {
+/* const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Node!\n'
   res.end(msg);
-});
+}); */
 
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
 
-router.get('/test', function(req,res,next){
-      res.send('Response Received!');
-});
+router.get('/', function (req, res) {
+  res.send('Wiki home page');
+})
+
+// About page route.
+router.get('/about', function (req, res) {
+  res.send('About this wiki');
+})
