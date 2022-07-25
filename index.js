29e4a38b312e5f25f663b8d0d9c290ed733c75df
Navneet Golwalkar <navneet@akpsolutionsinc.com>
2022-07-25 17:19:03 +0530

    Created a test HTTP request

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+const express = require('express');
+const router = express.Router();
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -9,4 +11,8 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
+});
+
+router.get('/test', function(req,res,next){
+      res.send('Response Received!');
 });
